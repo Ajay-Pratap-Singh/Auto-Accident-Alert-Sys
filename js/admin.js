3e4d921f44522d8d4526072804f789e37daa978e
Ajay-Pratap-Singh <ajay.snp99@gmail.com>
2020-05-17 16:29:18 +0530

    add under work

--- js/admin.js
+++ js/admin.js
@@ -11,6 +11,8 @@ const ambulances = dbRoot.child('Ambulances');
 const policeStations = dbRoot.child('PoliceStations');
 const vehicles = dbRoot.child('Vehicles');
 const accidents = dbRoot.child('Accidents');
+let editingRecordID;
+let additionActive;
 
 const editSVG='<svg title="edit" class="edit-row edit bi bi-pencil" width="1.6em" height="1.6em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M11.293 1.293a1 1 0 011.414 0l2 2a1 1 0 010 1.414l-9 9a1 1 0 01-.39.242l-3 1a1 1 0 01-1.266-1.265l1-3a1 1 0 01.242-.391l9-9zM12 2l2 2-9 9-3 1 1-3 9-9z" clip-rule="evenodd"/><path fill-rule="evenodd" d="M12.146 6.354l-2.5-2.5.708-.708 2.5 2.5-.707.708zM3 10v.5a.5.5 0 00.5.5H4v.5a.5.5 0 00.5.5H5v.5a.5.5 0 00.5.5H6v-1.5a.5.5 0 00-.5-.5H5v-.5a.5.5 0 00-.5-.5H3z" clip-rule="evenodd"/></svg>';
 const deleteSVG='<svg title="delete" class="delete-row delete bi bi-trash" width="1.6em" height="1.6em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M5.5 5.5A.5.5 0 016 6v6a.5.5 0 01-1 0V6a.5.5 0 01.5-.5zm2.5 0a.5.5 0 01.5.5v6a.5.5 0 01-1 0V6a.5.5 0 01.5-.5zm3 .5a.5.5 0 00-1 0v6a.5.5 0 001 0V6z"/><path fill-rule="evenodd" d="M14.5 3a1 1 0 01-1 1H13v9a2 2 0 01-2 2H5a2 2 0 01-2-2V4h-.5a1 1 0 01-1-1V2a1 1 0 011-1H6a1 1 0 011-1h2a1 1 0 011 1h3.5a1 1 0 011 1v1zM4.118 4L4 4.059V13a1 1 0 001 1h6a1 1 0 001-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" clip-rule="evenodd"/></svg>';
@@ -119,22 +121,54 @@ window.addEventListener("click",e=>{
         });
     }
     else if(e.target.classList.contains('edit-row')){
-        console.log(e.target.parentNode.parentNode.id);
+        document.querySelector('#input-appear h1').innerHTML="Edit";
+        const idElement=e.target.parentNode.parentNode.id;
+        const values=e.target.parentNode.parentNode.querySelectorAll('td');
+        if(editingRecordID || document.querySelector('#'+editingRecordID)){
+            document.querySelector('#'+editingRecordID).querySelector('.butt').innerHTML=editSVG+deleteSVG;
+        }
+        editingRecordID=idElement;
         document.querySelector('body').style.gridTemplateColumns='auto 15fr 7.5fr';
         document.querySelector('#input-appear').style.display='block';
         const type=e.target.parentNode.parentNode.classList[0];
-        if(type=='Ambulances'){
-
+        document.querySelector('#id').value=values[0].innerHTML;
+        document.querySelector('#contact').value=values[2].innerHTML;
+        if(type=='Ambulances' || type=='PoliceStations'){
+            document.querySelector('#name-input').style.display='none';
+            document.querySelector('#loc-input').style.display='block';
+            const latLong=values[1].innerHTML.split(': ');
+            document.querySelector('#lat').value=latLong[1].split('<')[0];
+            document.querySelector('#long').value=latLong[2];
+        }else if(type=='Vehicles'){
+            document.querySelector('#name-input').style.display='flex';
+            document.querySelector('#loc-input').style.display='none';
+            document.querySelector('#owner-name').value=values[1].innerHTML;
         }
         e.target.parentNode.innerHTML='<span class="cancel-edit">Cancel</span>';
     }
     else if(e.target.parentNode.classList.contains('edit-row')){
-        console.log(e.target.parentNode.parentNode.parentNode.id);
+        document.querySelector('#input-appear h1').innerHTML="Edit";
+        const idElement=e.target.parentNode.parentNode.parentNode.id;
+        const values=e.target.parentNode.parentNode.parentNode.querySelectorAll('td');
+        if(editingRecordID || document.querySelector('#'+editingRecordID)){
+            document.querySelector('#'+editingRecordID).querySelector('.butt').innerHTML=editSVG+deleteSVG;
+        }
+        editingRecordID=idElement;
         document.querySelector('body').style.gridTemplateColumns='auto 15fr 7.5fr';
         document.querySelector('#input-appear').style.display='block';
         const type=e.target.parentNode.parentNode.parentNode.classList[0];
-        if(type=='Ambulances'){
-
+        document.querySelector('#id').value=values[0].innerHTML;
+        document.querySelector('#contact').value=values[2].innerHTML;
+        if(type=='Ambulances' || type=='PoliceStations'){
+            document.querySelector('#name-input').style.display='none';
+            document.querySelector('#loc-input').style.display='block';
+            const latLong=values[1].innerHTML.split(': ');
+            document.querySelector('#lat').value=latLong[1].split('<')[0];
+            document.querySelector('#long').value=latLong[2];
+        }else if(type=='Vehicles'){
+            document.querySelector('#name-input').style.display='flex';
+            document.querySelector('#loc-input').style.display='none';
+            document.querySelector('#owner-name').value=values[1].innerHTML;
         }
         e.target.parentNode.parentNode.innerHTML='<span class="cancel-edit">Cancel</span>';
     }
@@ -142,6 +176,20 @@ window.addEventListener("click",e=>{
         document.querySelector('#input-appear').style.display='none';
         document.querySelector('body').style.gridTemplateColumns='auto 15fr';
         e.target.parentNode.innerHTML=editSVG+deleteSVG;
+    }else if(e.target.classList.contains('add-butt')){
+        document.querySelector('body').style.gridTemplateColumns='auto 15fr 7.5fr';
+        document.querySelector('#input-appear').style.display='block';
+        document.querySelector('#id-input').style.display='none';
+        e.target.innerHTML='Cancel Add';
+        e.target.style.background='crimson';
+        additionActive = e.target.add;
+        if(additionActive=='Ambulances'){
+            document.querySelector('#name-input').style.display='none';
+            document.querySelector('#loc-input').style.display='block';
+        }else if(additionActive=='Vehicles'){
+            document.querySelector('#name-input').style.display='flex';
+            document.querySelector('#loc-input').style.display='none';
+        }
     }
 })
 
