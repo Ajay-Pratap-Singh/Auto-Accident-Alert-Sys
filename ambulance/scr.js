5af1a7f78d8c287ee0a2c60fee04f2f16f3921f8
Ajay-Pratap-Singh <ajay.snp99@gmail.com>
2020-05-20 16:22:55 +0530

    ambulance location done

--- ambulance/scr.js
+++ ambulance/scr.js
@@ -6,15 +6,26 @@ ambulance.on('value',snap=>{
   console.log(snap.val());
 })
 
-var map;
-var marker;
-
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
+  var map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 0, lng: 0 },
     zoom: 2,
   });
-  marker=new google.maps.Marker({position:{ lat: 0, lng: 0 },map,icon:'../img/ambulance.png'});
+  var marker=new google.maps.Marker({position:{ lat: 0, lng: 0 },map,icon:'../img/ambulance.png'});
+  
+  window.setTimeout(function(){
+    navigator.geolocation.getCurrentPosition((position) => {
+      let pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      };
+      map.setCenter(pos);
+      map.setZoom(16);
+      console.log('a');
+      marker.setPosition(pos);
+      ambulance.child('currLocation').set({lat:pos.lat,long:pos.lng});
+    });
+  }, 5000);
 }
 
 window.setTimeout(function(){
