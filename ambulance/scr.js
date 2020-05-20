fef6442178151578a2a781f23c548685cb28b92e
Ajay-Pratap-Singh <ajay.snp99@gmail.com>
2020-05-20 15:48:01 +0530

    ambulance location done

---
+++ ambulance/scr.js
@@ -0,0 +1,32 @@
+const ambulanceID='-M7XKs39Zu_cLRjZrmbN';
+const dbRoot = firebase.database().ref();
+const ambulance = dbRoot.child('Ambulances/'+ambulanceID);
+
+ambulance.on('value',snap=>{
+  console.log(snap.val());
+})
+
+var map;
+var marker;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 0, lng: 0 },
+    zoom: 2,
+  });
+  marker=new google.maps.Marker({position:{ lat: 0, lng: 0 },map,icon:'../img/ambulance.png'});
+}
+
+window.setTimeout(function(){
+  navigator.geolocation.getCurrentPosition((position) => {
+    let pos = {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude,
+    };
+    map.setCenter(pos);
+    map.setZoom(16);
+    console.log('a');
+    marker.setPosition(pos);
+    ambulance.child('currLocation').set({lat:pos.lat,long:pos.lng});
+  });
+}, 5000);
